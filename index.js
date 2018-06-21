b98754ca82e672eb280870da60e88947f106fc09
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-06-21 12:41:52 +0200

    Finished 17.5

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('assets'));
+app.use('/store', function(req, res, next) {
+	console.log('Jestem pośrednikiem przy żądaniu do /store.');
+	next();
+});
 
 app.get('/', function (req, res) {
-	res.sendFile('./index.html');
+	res.send('Hello World!');
 });
 
-app.get('/userform', function (req, res) {
-	const response = {
-		first_name: req.query.first_name,
-		last_name: req.query.last_name
-	};
-	res.end(JSON.stringify(response));
+app.get('/store', function (req, res) {
+	res.send('To jest sklep');
 });
 
-let server = app.listen(3000, 'localhost', function() {
-	let host = server.address().address;
-	let port = server.address().port;
-
-	console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
+app.listen(3000);
