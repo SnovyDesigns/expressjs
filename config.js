1c343cf0d4e8eed3f17d96ed15cd15f53ca48aa5
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-06-23 11:05:04 +0200

    Finished 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '584045697476-mvke99tpl1mt6p23f7kb7j7el6lhijkh.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'CxPtFq48Zzl9ZS_0ltr0CL4I',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- index.js
+++ index.js
@@ -1,22 +1,62 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
 const app = express();
+let googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+	clientID: config.GOOGLE_CLIENT_ID,
+	clientSecret:config.GOOGLE_CLIENT_SECRET,
+	callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+	googleProfile = {
+		id: profile.id,
+		displayName: profile.displayName
+	};
+	cb(null, profile);
+}
+));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
-app.use('/auth/google', express.static('views'));
+app.use('/logged/', express.static('views'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res){
 	res.render('index', {
-		name: 'My dynamic Pug site',
-		url: '/auth/google'
+		name: 'Google Auth - Main site',
+		url: '/auth/google',
+		user: req.user
 	});
 });
 
-app.get('/auth/google', function(req, res){
-	res.render('auth', {
-		name: 'Google Auth'
-	});
+app.get('/logged', function(req, res){
+	res.render('logged', { 
+		name: 'Google Auth - Logged',
+		user: googleProfile });
 });
+
+app.get('/auth/google',
+	passport.authenticate('google', {
+		scope : ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback',
+	passport.authenticate('google', {
+		successRedirect : '/logged',
+		failureRedirect: '/'
+	}));
 
 app.listen(3000);
 app.use(function (req, res, next) {
